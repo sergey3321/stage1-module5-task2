6cb1a214192bbfa13cfc1d8df065b109ff128777
sergey3321 <mail.sergey21@gmail.com>
2023-12-14 20:14:41 +0300

    stage1-module5-task2

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
+    List<Integer> newList = new ArrayList<>();
+
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        if (divider == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
+        }
+
+        return arg -> {
+            for (Integer i : arg) {
+                newList.add(i / divider);
+            }
+
+            return newList;
+        };
     }
 }
